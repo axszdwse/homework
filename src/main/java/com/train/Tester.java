ad50b208164a44c9b114efe6a48be7ff8c951a88
axszdwse <a0284659570@gmail.com>
2019-03-13 02:41:17 +0800

    homework2 correct

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -4,12 +4,18 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        System.out.println("Please enter number of tickets: ");
-        Scanner scanner=new Scanner(System.in);
-        int tickets=scanner.nextInt();
-        System.out.println("How many round-trip tickets: ");
-        int round=scanner.nextInt();
-        Ticket tic=new Ticket(tickets,round);
-        tic.print();
+        int tickets = 0;
+        while (tickets != -1) {
+            System.out.print("Please enter number of tickets:");
+            Scanner scanner = new Scanner(System.in);
+            tickets = scanner.nextInt();
+            if(tickets==-1){
+                break;
+            }
+            System.out.print("How many round-trip tickets:");
+            int round = scanner.nextInt();
+            Ticket tic = new Ticket(tickets, round);
+            tic.print();
+        }
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -3,20 +3,26 @@ package com.train
 import java.util.*
 
 fun main(args: Array<String>) {
-    val scanner=Scanner(System.`in`)
-    print("Please enter number of tickets:")
-    var ticket=scanner.nextInt()
-    print("How many round-trip tickets:")
-    var round=scanner.nextInt()
-    var tester=TesterKotlin(ticket,round)
-    tester.print()
+    var tickets=0
+    while (tickets!=-1) {
+        val scanner = Scanner(System.`in`)
+        print("Please enter number of tickets:")
+        tickets = scanner.nextInt()
+        if(tickets==-1){
+            break
+        }
+        print("How many round-trip tickets:")
+        var round = scanner.nextInt()
+        var tester = TesterKotlin(tickets, round)
+        tester.print()
+    }
 }
 
 class TesterKotlin(var tickets:Int,var round:Int){
     fun print(){
         var total=(round*2000*9/10)+(tickets-round)*1000
-        print("Total tickets:"+tickets+"\n")
-        print("Round-trip:"+round+"\n")
-        print("Total:"+total)
+        println("Total tickets:$tickets")
+        println("Round-trip:$round")
+        println("Total:$total")
     }
 }
